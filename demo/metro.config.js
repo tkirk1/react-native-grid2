2517bed26f66d9951aeab7ed5db620df9133aad8
thomaskirk <TKirk@trustredline.onmicrosoft.com>
2025-06-28 23:10:24 +0100

    Expo Go fix for demo

--- demo/metro.config.js
+++ demo/metro.config.js
@@ -3,20 +3,9 @@ const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-// Add the monorepo root to the watchFolders
-config.watchFolders = [
-  path.resolve(__dirname, '../../'),
-];
-
 // Add the packages to the resolver
 config.resolver.nodeModulesPaths = [
   path.resolve(__dirname, 'node_modules'),
-  path.resolve(__dirname, '../../node_modules'),
 ];
-
-// Add alias for local package
-config.resolver.alias = {
-  'react-native-grid2': path.resolve(__dirname, '../../packages/grid2/lib'),
-};
 
 module.exports = config;
